27e7f3a58f205169a293635081a525fc533c0796
Alex Kubkin <ioc0@yandex.ru>
2024-08-19 16:26:09 +0300

    page 228 p2

--- headfirst/average2.go
+++ headfirst/average2.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
+func check(err error) {
+	log.Fatal(err)
+}
 func main() {
-	fmt.Println(os.Args[1:])
+	arguments := os.Args[1:]
+	var sum float64 = 0
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		check(err)
+		sum += number
+	}
+	fmt.Println(sum)
+	fmt.Printf("Average: %0.2f \n", sum/float64(len(arguments)))
+
 }
